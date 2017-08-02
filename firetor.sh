8e6ab2a53fde3eda31b27b3d13f87713dbf68d4d
MRoci <mroci@bruttocarattere.org>
2017-08-02 13:04:31 +0200

    Clean stuff on exit

--- firetor.sh
+++ firetor.sh
@@ -8,10 +8,13 @@ else
   sudo ip link set dev $NAME up
   sudo ip addr add 10.200.1.1/24 dev $NAME
   sudo iptables -t nat -A  PREROUTING -i $NAME -p udp --dport 53 -j REDIRECT --to-ports 5353
-
-  # traffic througth tor
   sudo iptables -t nat -A PREROUTING -i $NAME -p tcp --syn  -j REDIRECT --to-ports 9040    
   sudo iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 fi
 
 firejail --net=$NAME $*
+
+sudo ifconfig $NAME down
+sudo brctl delbr $NAME
+sudo iptables -t nat -D PREROUTING -i $NAME -p udp --dport 53 -j REDIRECT --to-ports 5353
+sudo iptables -t nat -D PREROUTING -i $NAME -p tcp --syn  -j REDIRECT --to-ports 9040
